d24c265f3b6883330e72d94f390af57ef08ffdbf
ashley <ashley@swings.io>
2023-09-21 16:21:49 +0100

    latest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import './App.css'
+
+import { Provider, useSelector, useDispatch } from 'react-redux'
+import InsiderTrading from './Components/insiderTrading'; 
+import TradingView from './Components/tradingView'; 
+import StockProfile from './Components/stockProfile'; 
+import Autocomplete from './Components/autocomplete';
+
+import store from './store'
 
 function App() {
+
+  const dispatch = useDispatch()
+  const currentSymbol = useSelector(state => state.currentSymbol)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+
+      {/* This should be in its own component: TODO */}
+      <div className="App">
+        {/* <h2>Current symbol is {currentSymbol}</h2>
+        <select onChange={(e) => dispatch(changeSymbol(e.target.value))} defaultValue={currentSymbol}>
+          <option value="AAPL">AAPL</option>
+          <option value="TSLA">TSLA</option>
+          <option value="GOOGL">GOOGL</option>
+          <option value="AMZN">AMZN</option>
+        </select>
+
+<br /><br /><br /><br /><br /> */}
+        <br /><br /><br /><br /><br /> 
+        <Autocomplete />
+
+
+        <StockProfile currentSymbol={currentSymbol} /> 
+        <InsiderTrading currentSymbol={currentSymbol} /> 
+        <div style={{height: '500px'}}>
+          <TradingView currentSymbol={currentSymbol} />
+        </div>
+        
+      </div>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
